5f1589264c8485131b40709a3a82712cf5d790f5
Егор Ершов <poleqqq@gmail.com>
2022-06-13 00:04:28 +0300

    change sql query

---
+++ app/app/alembic/versions/20220612_96ffaa717a5d.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 96ffaa717a5d
+Revises: bf22a6e45d7e
+Create Date: 2022-06-12 23:07:26.166040
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '96ffaa717a5d'
+down_revision = 'bf22a6e45d7e'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
--- app/app/views/default.py
+++ app/app/views/default.py
@@ -1,5 +1,6 @@
 from paginate_sqlalchemy import SqlalchemyOrmPage
 from pyramid.view import view_config
+from sqlalchemy import and_
 
 from .. import models
 
@@ -15,7 +16,13 @@ def _parse_page_param(request):
 @view_config(route_name='home', renderer='app:templates/documents.jinja2')
 def index_view(request):
     page = _parse_page_param(request) or 1
-    query = request.dbsession.query(models.Document)
+    query = request.dbsession.query(models.Document, models.User, models.Place, models.Scene, models.Visit) \
+        .select_from(models.Document) \
+        .join(models.User) \
+        .join(models.Place) \
+        .join(models.Visit, and_(models.Document.visit_id == models.Visit.id)) \
+        .join(models.Scene)
+
     paginated_queryset = SqlalchemyOrmPage(query, page=page, items_per_page=DEFAULT_PAGE_SIZE)
 
     return {'paginated_documents': paginated_queryset}
