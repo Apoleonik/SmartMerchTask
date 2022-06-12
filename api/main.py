69b3f38c31374de236997bc6b79481a70395f644
Егор Ершов <poleqqq@gmail.com>
2022-06-12 22:56:20 +0300

    fix upload error

--- api/main.py
+++ api/main.py
@@ -12,19 +12,21 @@ load_dotenv()
 CONNECTION_URL = os.getenv('CONNECTION_URL')
 API_PORT = int(os.getenv('API_PORT'))
 
+engine = create_async_engine(CONNECTION_URL, pool_size=1, max_overflow=0)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
 
 async def handle_upload(request):
-    engine = create_async_engine(CONNECTION_URL)
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         async with session.begin():
             uploaded_document = await request.json()
             await create_document_from_uploaded_json(session=session, uploaded_document=uploaded_document)
-    await engine.dispose()
     return web.Response()
 
 
 if __name__ == '__main__':
     app = web.Application()
-    app.add_routes([web.post('/upload/document/', handle_upload)])
+
+    app.router.add_route(method='post', path='/upload/document/', handler=handle_upload)
+
     web.run_app(app, port=API_PORT)
