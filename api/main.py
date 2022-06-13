07375f4e2bc5f8e6aab45466e3709d08151a993a
Егор Ершов <poleqqq@gmail.com>
2022-06-13 13:22:36 +0300

    add handle integrity error

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ import os
 
 from aiohttp import web
 from dotenv import load_dotenv
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
@@ -20,7 +21,10 @@ async def handle_upload(request):
     async with async_session() as session:
         async with session.begin():
             uploaded_document = await request.json()
-            await create_document_from_uploaded_json(session=session, uploaded_document=uploaded_document)
+            try:
+                await create_document_from_uploaded_json(session=session, uploaded_document=uploaded_document)
+            except IntegrityError:
+                return web.Response(status=500)
     return web.Response()
 
 
